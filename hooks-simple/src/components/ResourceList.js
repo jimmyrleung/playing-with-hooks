3b9210dc391b83140c6c283f9df052ff00e707a4
Jimmy Leung <jimmy.rl19@hotmail.com>
2019-06-20 17:08:45 -0300

    fetchList function inner useEffect

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -4,23 +4,33 @@ import axios from 'axios';
 const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResources = async () => {
-    const url = `${process.env.REACT_APP_BASE_API_URL}/${resource}`;
-
-    const resources = await axios.get(url);
-    await setResources(resources.data);
-  }
-
   // This method replaces the 'componentDidMount' and the 'componentDidUpdate'
   // lifecycle methods. Its mechanism also prevents the infinite loop of
   // 'componentDidUpdate'
   useEffect(
     () => {
-      fetchResources();
+
+      // You can isolate this one or use this nasty syntax :) (you'll find)
+      // a lot of useEffects with that syntax
+      (async () => {
+        const url = `${process.env.REACT_APP_BASE_API_URL}/${resource}`;
+
+        const resources = await axios.get(url);
+        await setResources(resources.data);
+      })()
     },
     // This array kind of "monitors" this property. If it changes, our
     // arrow function is called.
-    [resource]
+    // Obs 1: Objects might trigger the arrow function because it can be another 
+    // reference, even if the old obj and the new has the same keys and values.
+
+    // Obs 2: Always pass this array, if you don't pass (even when empty) it will
+    // work as componentDidUpdate and you might end in a infinite loop
+
+    // Obs 3: In arrays, it will compare th length and each value from the array, 
+    // so if you pass the same array twice (different references) it won't trigger 
+    // the arrow func.
+    [resource],
   )
 
   return (
